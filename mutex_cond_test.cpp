de5a0e8c9167991cd20542848f9c6e4a72f5b42d
100156994 <100156994@qq.com>
2020-02-08 00:51:41 +0800

    mutextest

---
+++ mutex_cond_test.cpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include"Thread.h"
+#include<string>
+#include <unistd.h>
+
+static int num=0;
+MutexLock mutex;
+
+void threadFunc()
+{
+    int k=10;
+    while(k--)
+    {
+         MutexLockGuard lock(mutex);
+        ++num;
+        printf("tid=%d num=%d\n", CurrentThread::tid(),num);
+    }
+
+}
+
+
+int main()
+{
+    Thread t1=Thread(threadFunc());
+    Thread t2=Thread(threadFunc());
+    t1.start();
+    t2.start();
+
+    t1.join();
+    t2.join();
+}
